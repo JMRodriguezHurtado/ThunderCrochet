2c16772b4ead1414bc84a08e36a58845a1866f7d
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2024-02-02 23:18:38 -0600

    [update] changes in general

--- thundercrochet/thundercrotchet.session.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    todoslosproductos.id AS todoslosproductos_id,
-    productos.nombre AS producto_nombre,
-    precios.precio AS precio_valor,
-    stock.stock AS stock_valor
-FROM todoslosproductos
-JOIN productos ON todoslosproductos.nombre_id = productos.id
-JOIN precios ON todoslosproductos.precio_id = precios.id
-JOIN stock ON todoslosproductos.stock_id = stock.id;
