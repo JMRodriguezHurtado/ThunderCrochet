54daa15f83db2883d0c6fd9034b6790f34c06c75
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2024-02-08 20:00:14 -0600

    [update] arquitecture of the backend

---
+++ thundercrochet/thundercrochet/server/src/db.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const fs = require('fs');
+const path = require('path');
+const {
+  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
+} = process.env;
+
+
+ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+    logging: false, 
+    native: false, 
+  });
+
+// const sequelize = new Sequelize(DB_DEPLOY, {
+
+//   logging: false, 
+//   native: false, 
+// });
+
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+  });
+
+
+modelDefiners.forEach(model => model(sequelize));
+
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+const { Reviews, Purchases, Products, User} = sequelize.models;
+
+User.belongsToMany(Products, {through: "user_products"});
+Products.belongsToMany(User, {through: "user_products"});
+
+Purchases.hasMany(Reviews, {foreignKey: "purchaseId", targetKey: "id"});
+Reviews.belongsTo(Purchases, {foreignKey: "purchaseId", sourceKey: "id"});
+
+Products.hasOne(Purchases, { foreignKey: "productsId", sourceKey: "id" });
+Purchases.belongsToMany(Products, { through: "product_purchases"});
+
+User.hasMany(Purchases, { foreignKey: "userId", sourceKey: "id" });
+Purchases.belongsTo(User, { foreignKey: "userId", sourceKey: "id" });
+
+User.hasMany(Reviews, { foreignKey: "userId", sourceKey: "id" });
+Reviews.belongsTo(User, { foreignKey: "userId", targetKey: "id" });
+
+Products.hasMany(Reviews, { foreignKey: "productsId", sourceKey: "id" });
+Reviews.belongsTo(Products, { foreignKey: "productsId", targetKey: "id" });
+
+module.exports = {
+  user_products,
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+};
